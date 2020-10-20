9aae80288f8f1d55081751faaa730ca7b04d5bf2
Shameer Hasan <shasan@shasan.ca>
2020-10-20 00:32:41 -0400

    Working on dynamodb persistence layer and import of data.

--- handler.py
+++ handler.py
@@ -2,13 +2,14 @@ import json
 import requests
 import logging
 import pprint
+import boto3
 
 log = logging.getLogger("COVID")
 
 
 def covid(event, context):
     resp = requests.get(
-        'https://data.ontario.ca/api/3/action/datastore_search?resource_id=8b6d22e2-7065-4b0f-966f-02640be366f2&limit=50&filters={"reported_date": "2020-10-16T00:00:00", "school_board": "Peel District School Board"}'
+        'https://data.ontario.ca/api/3/action/datastore_search?resource_id=8b6d22e2-7065-4b0f-966f-02640be366f2&limit=50&filters={"reported_date":"2020-10-16T00:00:00","school_board":"Peel District School Board"}'
     )
     body = resp.json()
 
@@ -22,6 +23,28 @@ def covid(event, context):
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
+
+
+def load(event, context):
+
+    resp = requests.get(
+        'https://data.ontario.ca/api/3/action/datastore_search?resource_id=8b6d22e2-7065-4b0f-966f-02640be366f2&limit=700&filters={"school_board":"Peel%20District%20School%20Board"}'
+    )
+    body = resp.json()
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('school-covid19-notify')
+
+    for record in body["result"]["records"]:
+        table.put_item(Item=record)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
 
 
 if __name__ == "__main__":
